41b25e59e3b4fae5b135b032d58daafea950642f
Denis Veselov <denis.veselov@NB-VESELOV.local>
2020-12-23 19:41:50 +0300

    remove fmt usage

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/golang/example/stringutil"
 )
 
 func main() {
-	fmt.Println(stringutil.Reverse("Hello, OTUS!"))
+	println(stringutil.Reverse("Hello, OTUS!"))
 }
